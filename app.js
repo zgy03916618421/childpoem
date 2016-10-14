468f27effb9305e26228a9c85b53af5d493b351c
zgy03916618421 <576428698@qq.com>
2016-10-14 15:38:02 +0800

    router

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
  * Created by Administrator on 2016/10/14.
  */
 var koa = require('koa');
+var router = require('./router/router');
 var app = koa();
 app.use(router.routes()).use(router.allowedMethods());
 app.listen()
