9a4e702c70a525b59bbaa0d8a85227c0824a5f32
zgy03916618421 <576428698@qq.com>
2016-11-10 11:43:07 +0800

    logger

--- app.js
+++ app.js
@@ -3,7 +3,9 @@
  */
 var koa = require('koa');
 var router = require('./router/router');
+var logger = require('koa-logger');
 var app = koa();
 require('./middleware/connectMongo');
+app.use(logger());
 app.use(router.routes()).use(router.allowedMethods());
 app.listen(10004)
